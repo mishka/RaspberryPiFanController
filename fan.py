7852c3fca777e236a22e43084890992fd24f7ccc
浪人 <mighxi@yahoo.com>
2020-10-17 11:11:27 +0300

    v0.2
    GPIO PIN state is not stable, therefore changed it with a manual switch inside the program.

--- fan.py
+++ fan.py
@@ -4,10 +4,11 @@ from time import sleep
 from os import popen
 
 PIN = 12
-THRESHOLD = 43
+THRESHOLD = 44
+CHIKUSHOU = 0
 
 def log(text):
-    current_date = date.today().strftime('%d/%m%y')
+    current_date = date.today().strftime('%d/%m/%y')
     current_time = datetime.now().strftime('%H:%M:%S')
     print(f'| {current_date} {current_time} | {text}')
 
@@ -21,11 +22,13 @@ fan = OutputDevice(PIN)
 while 1:
     temperature = get_temp()
 
-    if temperature > THRESHOLD and not fan.value:
+    if temperature > THRESHOLD and not CHIKUSHOU:
         fan.on()
-        log('Turning on the fan.')
-    elif fan.value and temperature < THRESHOLD:
+        CHIKUSHOU = 1
+        log('ON!')
+    elif CHIKUSHOU and temperature < THRESHOLD:
         fan.off()
-        log('Turning off the fan.')
+        CHIKUSHOU = 0
+        log('OFF!')
 
     sleep(5)
